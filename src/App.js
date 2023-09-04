02d5fe3bc542f0bdd9878978e4b061302bf6cf87
myw823 <47249506+myw823@users.noreply.github.com>
2023-09-04 22:49:15 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import "./App.css";
+
+const App = () => {
+    return (
+        <Box width="400px">App</Box>
+    );
+}
+
+export default App;
